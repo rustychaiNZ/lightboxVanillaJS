a51035c45b99824dfc5271e1b1713b34fb730278
Russell grant Johnson <932400859@bun2-206-14.local>
2019-11-05 12:44:55 +1300

    added bootstrap

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // Opens the modal
 function openModal() {
-  	document.getElementById("myModal").style.display = "block";
+	document.getElementById("myModal").style.display = "block";
 }
 
 // Close the modal
 function closeModal() {
-  	document.getElementById("myModal").style.display = "none";
+	document.getElementById("myModal").style.display = "none";
 }
 
 var slideIndex = 1;
@@ -13,35 +13,32 @@ showSlides(slideIndex);
 
 // forward and back controls
 function plusSlides(n) {
-  	showSlides(slideIndex += n);
+	showSlides(slideIndex += n);
 }
 // Thumbnail image controls
 function currentSlide(n) {
- 	showSlides(slideIndex = n);
+	showSlides(slideIndex = n);
 }
 function showSlides(n) {
-  	var i;
-  	var slides = document.getElementsByClassName("my-slides");
-  	var dots = document.getElementsByClassName("demo");
-  	var captionText = document.getElementById("caption");
-  	
-  	if (n > slides.length) {
-  		slideIndex = 1;
-  	}
-
-  	if (n < 1) {
-  		slideIndex = slides.length;
-  	}
-
-  	for (i = 0; i < slides.length; i++) {
-  	  slides[i].style.display = "none";
-  	}
-
-  	for (i = 0; i < dots.length; i++) {
-  	  dots[i].className = dots[i].className.replace(" active", "");
-  	}
-  	
-  	slides[slideIndex-1].style.display = "block";
-  	dots[slideIndex-1].className += " active";
-  	captionText.innerHTML = dots[slideIndex-1].alt;
+	var i;
+	var slides = document.getElementsByClassName("my-slides");
+	var dots = document.getElementsByClassName("demo");
+	var captionText = document.getElementById("caption");
+	
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+	captionText.innerHTML = dots[slideIndex-1].alt;
 }
